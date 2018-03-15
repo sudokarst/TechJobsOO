7d50f49a514c2b726fc641f9754f9061b014769f
sudokarst <sudokarst@gmail.com>
2018-03-15 17:36:30 -0400

    DISPLAY SINGLE JOB, CREATE NEW JOBS functionality added

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -18,9 +19,10 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            // TODO #1X - get the Job with the given ID and pass it into the view
+            JobData jobData = JobData.GetInstance();
+            Job job = jobData.Find(id);
+            return View(job);
         }
 
         public IActionResult New()
@@ -35,7 +37,25 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                JobData jobData = JobData.GetInstance();
+                Employer employer = jobData.Employers.Find(newJobViewModel.EmployerID);
+                Location location = jobData.Locations.Find(newJobViewModel.LocationID);
+                CoreCompetency coreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID);
+                PositionType positionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID);
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = employer,
+                    Location = location,
+                    CoreCompetency = coreCompetency,
+                    PositionType = positionType
+                };
+                jobData.Jobs.Add(newJob);
 
+                return Redirect("/Job?id=" + newJob.ID.ToString());
+            }
             return View(newJobViewModel);
         }
     }
--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -8,14 +8,24 @@ namespace TechJobs.ViewModels
 {
     public class NewJobViewModel
     {
+        [StringLength(60, MinimumLength = 5)]
         [Required]
         public string Name { get; set; }
 
-        [Required]
+        [Required] // https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation#notes-on-the-use-of-the-required-attribute
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
-        // TODO #3 - Included other fields needed to create a job,
+        [Display(Name = "Location")]
+        public int LocationID { get; set; }
+
+        [Display(Name = "Core Competency")]
+        public int CoreCompetencyID { get; set; }
+
+        [Display(Name = "Position Type")]
+        public int PositionTypeID { get; set; }
+
+        // TODO #3X - Included other fields needed to create a job,
         // with correct validation attributes and display names.
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
@@ -36,9 +46,32 @@ namespace TechJobs.ViewModels
                 });
             }
 
-            // TODO #4 - populate the other List<SelectListItem> 
+            // TODO #4X - populate the other List<SelectListItem> 
             // collections needed in the view
 
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
         }
     }
 }
